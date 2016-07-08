2f9cec3ebb1111e53a4b38d38379b381b457e65c
joy <joy.jing1@gmail.com>
2016-07-08 10:37:07 -0700

    Style front signin/login splash page

--- frontend/components/login/login_page.jsx
+++ frontend/components/login/login_page.jsx
@@ -17,28 +17,29 @@ const LoginPage = React.createClass({
             <LoginForm/>
           </header>
 
-          <div className="logged-out-body">
-            <aside className="signup-left"/>
+          <div className="logged-out-body clearfix">
+            <aside className="signup-left clearfix">
 
-              <h1>Connect with friends and the world around you on Friendex</h1>
+              <h1 className="signed-out-header">Connect with friends and the world around you on Friendex.</h1>
 
               <ul className="logged-out-left">
                 <li>
-                  <img src="http://res.cloudinary.com/joyjing1/image/upload/v1467995306/icons/iconmonstr-share-2-240.png"></img>
+                  <img src="https://scontent-lax.xx.fbcdn.net/t39.2365-6/851565_602269956474188_918638970_n.png"></img>
                   <span className="login-bold">See photos and updates</span>
                   <span className="login-normal">from friends in News Feed</span>
                 </li>
                 <li>
-                  <img src="http://res.cloudinary.com/joyjing1/image/upload/v1467995306/icons/iconmonstr-share-2-240.png"></img>
+                  <img src="https://scontent-lax.xx.fbcdn.net/t39.2365-6/851585_216271631855613_2121533625_n.png"></img>
                   <span className="login-bold">Share whta's new</span>
                   <span className="login-normal">in your life on your Timeline</span>
                 </li>
                 <li>
-                  <img src="http://res.cloudinary.com/joyjing1/image/upload/v1467995306/icons/iconmonstr-share-2-240.png"></img>
+                  <img src="https://scontent-lax.xx.fbcdn.net/t39.2365-6/851558_160351450817973_1678868765_n.png"></img>
                   <span className="login-bold">Find more</span>
                   <span className="login-normal">of what you're looking for with Friendex Search</span>
                 </li>
               </ul>
+            </aside>
 
 
             <aside className="signup-right">
@@ -59,3 +60,6 @@ const LoginPage = React.createClass({
 });
 
 module.exports = LoginPage;
+
+//
+// src="https://scontent-lax.xx.fbcdn.net/t39.2365-6/851585_216271631855613_2121533625_n.png"
